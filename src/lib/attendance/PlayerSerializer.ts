fbe55b8fb78f84cdbf9f32249dde3f32845571c8
Kyle Scheuing <oppzippy@gmail.com>
2020-03-19 23:36:57 -0400

    Fix Raiders Only being case sensitive

--- src/lib/attendance/PlayerSerializer.ts
+++ src/lib/attendance/PlayerSerializer.ts
@@ -2,7 +2,7 @@ import Player from "./Player";
 
 class PlayerSerializer {
   public static serialize(player: Player): string {
-    return this.serializeHumanReadable(player);
+    return this.serializeHumanReadable(player).toLowerCase();
   }
 
   public static serializeHumanReadable(player: Player): string {
